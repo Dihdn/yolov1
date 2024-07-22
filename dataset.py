fea60f2bcd323c86cf04d3ea2ed15ec0a7dc83ac
stronger <kangyuh081125@163.com>
2024-07-22 15:54:39 +0800

    dataset.py完成

--- dataset.py
+++ dataset.py
@@ -5,20 +5,26 @@ from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
 import pandas as pd
 import numpy as np
+import PIL.Image as Image
 import os
 import utilities
 
+CLASS_LIST = ["apple", "banana", "orange"]
 
 data_path = "E:\\yolov1\\new_data\\train"
 
 # 制作dataset
 class new_data_dataset(Dataset):
-    def __init__(self, data_path, resize=(448, 448)):
+    def __init__(self, data_path, resize=(448, 448), map_size=(7, 7)):
         self.data_path = data_path
         self.resize = resize
+        self.map_size = map_size
+
         #获取路径中所有图片名称
-        self.img_path_names = os.listdir(path=data_path+"\\train_img")
-        self.xml_path_names = os.listdir(path=data_path+"\\train_xml")
+        img_path = data_path+"\\train_img"
+        xml_path = data_path+"\\train_xml"
+        self.img_path_names = os.listdir(path=img_path)
+        self.xml_path_names = os.listdir(path=xml_path)
 
     def __len__(self):
         # 返回数图片数量
@@ -26,13 +32,19 @@ class new_data_dataset(Dataset):
 
     def __getitem__(self, index):
         img_path = self.data_path + "\\train_img\\" + self.img_path_names[index]
-        xml_path = self.data_path + "\\train_xml\\" + self.img_path_names[index]
+        xml_path = self.data_path + "\\train_xml\\" + self.xml_path_names[index]
         # 将图片尺寸调整为448*448
         img = utilities.resize_img(img_path, self.resize)
-        return img
+        # 得到标签
+        label = utilities.get_label(xml_path=xml_path, img_size=img.size, resize=self.resize, map_size=self.map_size, class_list=CLASS_LIST)
+        return img, label
 
 
 if __name__ == "__main__":
     dataset = new_data_dataset(data_path)
-    a = dataset[0]
+    img, label = dataset[1]
+    Image._show(image=img)
+    print(label[3][2])
+    print(label[1][2])
+    print(label[2][1])
     print("a")
--- utilities.py
+++ utilities.py
@@ -1,19 +1,39 @@
 import os
 import string
+import torch
 import PIL.Image as Image
 import xml.etree.ElementTree as ET#zyz
+
+
 # 得到文件类型
 def get_file_type(path:str) -> str:
+    # path:文件名或路径
+    # return 文件类型
     path_split = path.split(".")
     return path_split[-1]
 
+
 # 得到图片名字
 def get_img_name(path):
+    # path: 图片的路径
+    # return 图片名字
     path_split = path.split(".")
     return path_split[-2]
 
+
+# 调整图像尺寸
+def resize_img(img_path, new_size:tuple):
+    # img_path:文件路径  new_size:调整后的图片尺寸
+    # return 调整尺寸后的图片
+    origin_img = Image.open(img_path)
+    img = origin_img.resize(new_size)
+    return img
+
+
 # 将原图坐标映射到(s*s)图像中
 def get_map_coordinate(origin_coord:tuple, origin_size:tuple, map_size:tuple) -> tuple:
+    # origin_coord:原图中的坐标 元组表示(x, y)  # origin_size:原始图片尺寸  元组表示(w, h)  # map_size:要映射到图片的层次 元组表示(w, h)
+    # return s*s中的坐标(x, y)
     origin_x, origin_y = origin_coord
     origin_w, origin_h = origin_size
     map_w, map_h = map_size
@@ -21,15 +41,67 @@ def get_map_coordinate(origin_coord:tuple, origin_size:tuple, map_size:tuple) ->
     map_x, map_y = scale_w*origin_x, scale_h*origin_y
     return (map_x, map_y)
 
-# 调整图像尺寸
-def resize_img(img_path, new_size:tuple):
-    origin_img = Image.open(img_path)
-    img = origin_img.resize(new_size)
-    return img
+
+# 将原图中的框映射到s*s图像中
+def get_map_size(origin_size:tuple, map_size:tuple, box_size:tuple):
+    # origin_size:原始图像尺寸 元组表示(w, h)  map_size:要映射的图像尺寸 元组表示(w, h)  box_size:原始图像中框的尺寸 元组表示(w, h)
+    # return map_size中图像的尺寸 元组表示(w, h)
+    scale_w, scale_h = map_size[0]/origin_size[0], map_size[1]/origin_size[1]
+    map_box_size = (box_size[0]*scale_w, box_size[1]*scale_h)
+    return map_box_size
+
+
+# 将类别转成one-hot类
+def convert_class_to_one_hot(class_name:str, class_list:list):
+    # class_name:类别名称 class_list：所有类别组成的列表
+    # return 类别的one-hot类型
+    index = class_list.index(class_name)
+    one_hot = torch.zeros(size=(len(class_list), ))
+    one_hot[index] = 1
+    one_hot = one_hot.detach().tolist()
+    return one_hot
+
+
+# 分离小数与整数部分
+def separate_int_float(num):
+    # num:要分离的数
+    # return 整数和小数部分 元组表示(整数， 小数部分)
+    return (int(num), num%1)
 
 # 得到图像的标签
-def get_label(xml_path, img_size:tuple, map_size:tuple):
-    
+def get_label(xml_path, img_size:tuple, resize, map_size:tuple, class_list:list):
+    # xml_path:xml文件路径  img_size:原始图片尺寸 元组表示(w, h)  map_size:映射图片尺寸 元组表示(w, h)
+    # 得到图片相关信息
+    img_size, objects = get_size_box(xml_path=xml_path)
+    # 创建一个与map_size一样大的空白布
+    size = (map_size[0], map_size[1], 5+len(class_list))
+    blank_canvas = torch.zeros(size=size)
+    for obj in objects:
+        x_min, y_min, x_max, y_max, class_name = obj
+        # 框的宽高
+        bndbox_w, bndbox_h = x_max-x_min, y_max-y_min
+        # 将框的宽高映射到7*7图像中
+        box_w, box_h = get_map_size(origin_size=img_size, map_size=map_size, box_size=(bndbox_w, bndbox_h))
+        # 框的中心坐标映射到(448*448)图像中
+        central_x, central_y = get_map_coordinate(origin_coord=((x_min+x_max)/2, (y_min+y_max)/2), origin_size=img_size, map_size=resize)
+        # 计算中心点在7*7中的偏移量
+        map_x, map_y = central_x/(resize[0]/map_size[0]), central_y/(resize[1]/map_size[1])
+        map_x_int, map_x_float = separate_int_float(map_x)
+        map_y_int, map_y_float = separate_int_float(map_y)
+        # 将类别转成one-hot类型
+        one_hot = convert_class_to_one_hot(class_name=class_name, class_list=class_list)
+        confidence = 1
+        # 将标签数据填入对应的布中
+        label_obj = []
+        label_obj.append(map_x_float)
+        label_obj.append(map_y_float)
+        label_obj.append(box_w)
+        label_obj.append(box_h)
+        label_obj.append(confidence)
+        label_obj += one_hot
+        blank_canvas[map_x_int][map_y_int] = torch.tensor(label_obj)
+    return blank_canvas
+
 
 
 ###zyz
@@ -52,8 +124,8 @@ def convert(size, box):
     box_coordinates = [
         (int(left + x * scale), int(top + y * scale)) for x, y in box
     ]
-
     return box_coordinates
+
 def convert2(box):
     #将位置转化为（中心点，方框宽度，高度）并归一化
     #box=[xmin,ymin,xmax,ymax]
@@ -64,61 +136,31 @@ def convert2(box):
     w=box[2]-box[0]
     return (x,y,h,w)
 
-def get_size_box(file):
-    #从xml文件获取size（图片大小，多个方框,位置坐标）
-
-    xml_file = './new_data/train_xml/banana_9.xml'  # 替换为你的XML文件路径
-    in_file = open(xml_file, 'r')
+#从一个xml文件获取size（图片大小，多个方框,位置坐标）
+def get_size_box(xml_path):
+    #返回对应图片的信息(with,hight)[xmin,ymin,xmax,ymax,种类】
+    in_file = open(xml_path, 'r')
     tree = ET.parse(in_file)
     root = tree.getroot()
     size = root.find('size')
     w = int(size.find('width').text)
     h = int(size.find('height').text)
     boxs = []
-    p = 0
     for obj in root.iter('object'):
         cls = obj.find('name').text
-        print(cls + str(p))
-        p = p + 1
+        cls2=cls
         bndbox = obj.find('bndbox')
         xmin = int(bndbox.find('xmin').text)
         ymin = int(bndbox.find('ymin').text)
         xmax = int(bndbox.find('xmax').text)
         ymax = int(bndbox.find('ymax').text)
-        box = [xmin, ymin, xmax, ymax]
+        box = [xmin, ymin, xmax, ymax,cls2]
         boxs.append(box)
     return (w,h),boxs
 
-def resize_and_draw_box2(input_image_path, output_image_path):
-    # 打开图片
-    #output_image_path，转化大小后的图片保存路径
-    image = Image.open(input_image_path)
-    # 定义目标尺寸
-    target_size = (448, 448)
-    # 获取原始图片尺寸
-    original_width, original_height = image.size
-    # 计算宽和高的缩放比例
-    scale_width = target_size[0] / original_width
-    scale_height = target_size[1] / original_height
-    # 选择较小的缩放比例，保持宽高比
-    scale = min(scale_width, scale_height)
-    # 计算新的图片尺寸
-    new_size = (int(original_width * scale), int(original_height * scale))
-    # 缩小图片，保持宽高比
-    image = image.resize(new_size, Image.Resampling.LANCZOS)
 
-    # 创建一个新的黑色背景的图片
-    background = Image.new('RGB', target_size, (0, 0, 0))  # 黑色背景
-
-    # 计算粘贴位置使图片居中
-    left = (target_size[0] - new_size[0]) // 2
-    top = (target_size[1] - new_size[1]) // 2
-
-    # 将缩小后的图片粘贴到黑色背景图片上
-    background.paste(image, (left, top))
-    background.save(output_image_path)
 
 if __name__ == '__main__':
-    path = "E:\\yolov1\\new_data\\train\\train_img\\apple_1.jpg"
+    path = "E:\\yolov1\\new_data\\train\\train_img\\apple_2.jpg"
     a = resize_img(path, (448, 448))
     print("a")
